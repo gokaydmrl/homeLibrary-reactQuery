2d9dd403d59b6f8da6d88fd10d831a14e0e765b7
gokaydmrl <gokaydemirel96@gmail.com>
2022-07-24 13:11:54 +0300

    master

--- client/src/api/usePatchBook.jsx
+++ client/src/api/usePatchBook.jsx
@@ -1,3 +1,37 @@
+// import { useMutation, useQueryClient } from "react-query";
+// import patchBookAction from "./patchBookAction";
+
+// const usePatchBook = () => {
+//   const queryClient = useQueryClient();
+
+//   return useMutation(patchBookAction, {
+//     onMutate: async (bookToPatch) => {
+//       await queryClient.cancelQueries(["books", bookToPatch.id]);
+//       const previousBook = queryClient.getQueryData(["books", bookToPatch.id]);
+//       console.log("previous book", previousBook);
+//       console.log("book to patch", bookToPatch, "id'si", bookToPatch.id);
+//       queryClient.setQueryData(["books", {id: bookToPatch.id}], bookToPatch);
+//       return { previousBook, bookToPatch };
+//     },
+//     onError: (err, bookToPatch, context) => {
+//       queryClient.setQueryData(
+//         ["books", context.bookToPatch.id],
+//         context.previousBook
+//       )
+
+//       console.log("onerror aşamasında", bookToPatch);
+//       console.log("err", err);
+//     },
+//     onSettled: (bookToPatch) => {
+//       queryClient.invalidateQueries(["books", bookToPatch.id]);
+//       console.log("on settleddaki obje", bookToPatch);
+//     },
+//   });
+// };
+
+// export default usePatchBook;
+
+
 import { useMutation, useQueryClient } from "react-query";
 import patchBookAction from "./patchBookAction";
 
@@ -5,26 +39,11 @@ const usePatchBook = () => {
   const queryClient = useQueryClient();
 
   return useMutation(patchBookAction, {
-    onMutate: async (bookToPatch) => {
-      await queryClient.cancelQueries(["books", bookToPatch.id]);
-      const previousBook = queryClient.getQueryData(["books", bookToPatch.id]);
-      console.log("previous book", previousBook);
-      console.log("book to patch", bookToPatch, "id'si", bookToPatch.id);
-      queryClient.setQueryData(["books", bookToPatch.id], bookToPatch);
-      return { previousBook, bookToPatch };
-    },
-    onError: (err, bookToPatch, context) => {
-      queryClient.setQueryData(
-        ["books", context.bookToPatch.id],
-        context.previousBook
-      )
-
-      console.log("onerror aşamasında", bookToPatch);
-      console.log("err", err);
-    },
-    onSettled: (bookToPatch) => {
-      queryClient.invalidateQueries(["books", bookToPatch.id]);
-      console.log("on settleddaki obje", bookToPatch);
+    // Notice the second argument is the variables object that the `mutate` function receives
+    onSuccess: (data, bookToPatch) => {
+      console.log("bktopatch", bookToPatch);
+      queryClient.setQueryData(["books", { id: bookToPatch.id }], data);
+      console.log("data",data);
     },
   });
 };
